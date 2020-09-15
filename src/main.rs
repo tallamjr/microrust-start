97e89ab9e5730824ebaa2626efbe55fcbf99c83c
Tarek Allam <t.allam.jr@gmail.com>
2020-09-15 16:48:10 +0100

    Add building code from 'cortex-m0-rust' repo
    REF:
        https://github.com/rust-embedded/cortex-m-quickstart
    
    	new file:   .cargo/config.toml
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	new file:   build.rs
    	new file:   memory.x
    	new file:   openocd.cfg
    	new file:   openocd.gdb
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#![allow(unused_variables)]
 #![no_std]
 #![no_main]
 
-fn main() {
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
     loop {}
-}
 }
